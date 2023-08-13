2cfcc41148006a0c7645b93b575fa14219c3450d
Bradley Hower <bradleyhower@proton.me>
2023-08-13 12:30:57 -0700

    Problem three  solution

--- js/app.js
+++ js/app.js
@@ -51,12 +51,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sum_input_ab = sum(a, b)[0];
+
+  let mult_input_ab = multiply(a, b)[0];
+
+  let sumnums_three = sum(c, sum_input_ab)[0];
+  let sumnums_three_string = `${a} and ${b} and ${c} sum to ${sumnums_three}.`;
+  let productnums_three = multiply(c, mult_input_ab)[0];
+  let productnums_three_string = `The product of ${a} and ${b} and ${c} is ${productnums_three}.`;
+  return [sumnums_three, productnums_three, sumnums_three_string, productnums_three_string];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
